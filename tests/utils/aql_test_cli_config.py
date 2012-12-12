c7bb00ac9a98f0f33ddf224620bef8e413697c2b
menify <menify@a28edc5c-ec3e-0410-a3da-1b30b3a8704b>
2012-12-12 08:21:48 +0000

    Added tests for CLI Config
    git-svn-id: https://aqualid.googlecode.com/svn/aql@500 a28edc5c-ec3e-0410-a3da-1b30b3a8704b

--- tests/utils/aql_test_cli_config.py
+++ tests/utils/aql_test_cli_config.py
@@ -25,7 +25,23 @@ class TestCLIConfig( AqlTestCase ):
     config = CLIConfig( usage, cli_options, ["-j", "0", "-v", "foo", "-s32", "bv=release"])
     
     config.setDefault( 'jobs', 3 )
+    config.setDefault( 'size', 10 )
+    config.setDefault( 'new_size', 2 )
     self.assertTrue( config.jobs, 0 )
+    self.assertTrue( config.size, 32 )
+    self.assertTrue( config.new_size, 2 )
+    self.assertTrue( config.targets, ['foo'] )
+    self.assertTrue( config.values['bv'], 'release' )
+    
+    config.jobs = 10
+    self.assertTrue( config.jobs, 10 )
+    config.size = 20
+    self.assertTrue( config.size, 20 )
+    config.new_size = 1
+    self.assertTrue( config.new_size, 1 )
+    
+    config.setDefault( 'new_size', 30 )
+    self.assertTrue( config.new_size, 1 )
 
 #//===========================================================================//
 
