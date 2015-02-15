9e6c02f5a3abfc9c1790a2a7913351d5c6cda22b
menify <menify@gmail.com>
2015-02-16 02:18:36 +0300

    Updated Aqualid version

--- aql/main/aql_info.py
+++ aql/main/aql_info.py
@@ -22,7 +22,7 @@ class AqlInfo (object):
     self.name         = "Aqualid"
     self.module       = "aqualid"
     self.description  = "General purpose build tool."
-    self.version      = 0.52
+    self.version      = 0.53
     self.date         = None
     self.url          = 'https://github.com/aqualid'
     self.license      = "MIT License"
