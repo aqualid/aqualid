8d8d94ab87ba110c6042a51df7cf40ce687a1f83
menify <menify@gmail.com>
2015-04-19 10:27:58 +0300

    Added building examples to Travis CI

--- travis.sh
+++ travis.sh
@@ -1,5 +1,12 @@
 set -ex
 
 python tests/aql_tests.py
+python -c "import aql;import sys;sys.exit(aql.main())" -C make local
+python -c "import aql;import sys;sys.exit(aql.main())" -C examples/cpp_hello
+python -c "import aql;import sys;sys.exit(aql.main())" -C examples/cpp_generator
+python -c "import aql;import sys;sys.exit(aql.main())" -C examples/cpp_libs
+python -c "import aql;import sys;sys.exit(aql.main())" -C examples/cpp_libs_1
+python -c "import aql;import sys;sys.exit(aql.main())" -C examples/cpp_libs_2
+
 pep8 `find aql -name "[a-zA-Z]*.py"`
 #flake8 `find aql -name "[a-zA-Z]*.py"`
