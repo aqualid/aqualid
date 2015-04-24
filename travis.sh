5009b8ef1766aaa2b12816ae1846dd39349be8ee
menify <menify@gmail.com>
2015-04-25 01:42:13 +0300

    Fixed Travis CI script

--- travis.sh
+++ travis.sh
@@ -3,13 +3,13 @@ set -ex
 python tests/aql_tests.py
 python -c "import aql;import sys;sys.exit(aql.main())" -C make local -I $PWD/tools
 python -c "import aql;import sys;sys.exit(aql.main())" -C examples/cpp_hello -I $PWD/tools
-python -c "import aql;import sys;sys.exit(aql.main())" -C examples/cpp_hello -I $PWD/tools -B
+python -c "import aql;import sys;sys.exit(aql.main())" -C examples/cpp_hello -I $PWD/tools -R
 python -c "import aql;import sys;sys.exit(aql.main())" -C examples/cpp_generator -I $PWD/tools
-python -c "import aql;import sys;sys.exit(aql.main())" -C examples/cpp_generator -I $PWD/tools -B
+python -c "import aql;import sys;sys.exit(aql.main())" -C examples/cpp_generator -I $PWD/tools -R
 python -c "import aql;import sys;sys.exit(aql.main())" -C examples/cpp_libs -I $PWD/tools
 python -c "import aql;import sys;sys.exit(aql.main())" -C examples/cpp_libs_1 -I $PWD/tools
 python -c "import aql;import sys;sys.exit(aql.main())" -C examples/cpp_libs_2 -I $PWD/tools
-python -c "import aql;import sys;sys.exit(aql.main())" -C examples/cpp_libs_2 -I $PWD/tools -B
+python -c "import aql;import sys;sys.exit(aql.main())" -C examples/cpp_libs_2 -I $PWD/tools -R
 
 flake8 `find aql -name "[a-zA-Z]*.py"`
 #flake8 --max-complexity=7 `find aql -name "[a-zA-Z]*.py"`
